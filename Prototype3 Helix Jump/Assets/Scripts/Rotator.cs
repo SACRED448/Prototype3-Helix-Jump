c514427c1656fc45138b7c2c799441cd7c30cf25
unknown <Sergey>
2022-02-27 21:55:57 +0700

    Work from Sergey

--- Prototype3 Helix Jump/Assets/Scripts/Rotator.cs
+++ Prototype3 Helix Jump/Assets/Scripts/Rotator.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    public float rotationSpeed = 150f;
+    public float rotationSpeed = 10f;
 
     
     void Update()
@@ -21,7 +21,7 @@ public class Rotator : MonoBehaviour
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             float xDelta = Input.GetTouch(0).deltaPosition.x;
-            transform.Rotate(0, /*-xDelta **/ rotationSpeed /** Time.deltaTime*/, 0);
+            transform.Rotate(0, -xDelta * rotationSpeed * Time.deltaTime, 0);
         }
 
     }
